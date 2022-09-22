507ebd6140041a261d6d1559e7e6a12a837916c6
cosine0720 <cosine0720@gmail.com>
2022-09-22 13:48:41 +0800

    feat: add Database

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
+require("./config/mongoose")
+
 const urlShortener = require('./url_shortener')
-const Record = require('./models/record')
-
-
+const URL = require('./models/URL')
 
 const app = express()
 const PORT = process.env.PORT || 3000
-
-mongoose.connect(process.env.MONGODB_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-})
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
+
 app.use(express.urlencoded({ extended: true }))
 
 // 首頁
@@ -38,8 +23,22 @@ app.get('/',(req, res) => {
 // 網址結果
 app.post('/',(req, res) => {
   const url = req.body.url
-  const newUrl = urlShortener(url)
-  res.render('index', { url, newUrl })
+  const shortURL = urlShortener(url)
+
+  if (!url) return res.redirect("/")
+
+  URL.findOne({ originalURL: url })
+    .then(data =>
+      data ? data : URL.create({ shortURL, originalURL: url })
+    )
+    .then(data =>
+      res.render("index", {
+        url,
+        origin: req.headers.origin,
+        shortURL: data.shortURL,
+      })
+    )
+    .catch(error => console.error(error))
 })
 
 app.listen(PORT, () => {
